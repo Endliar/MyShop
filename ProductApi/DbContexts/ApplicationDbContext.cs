908ce5a939f315b4ee5ef63d4f987088f4eea658
endliar <endliar@yandex.ru>
2023-06-01 22:37:07 +0700

    Добавьте файлы проекта.

---
+++ ProductApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
